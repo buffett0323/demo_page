44032d65717dcbc52caaeac2d46070450100891a
buffett0323 <philip910323@gmail.com>
2025-06-03 20:13:54 +0800

    github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/demo_page/',
   server: {
     port: 3000,
   },
